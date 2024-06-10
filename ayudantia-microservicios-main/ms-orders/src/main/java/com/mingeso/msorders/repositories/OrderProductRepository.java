b8d5594227491549778f62c65a62e6d76b016ac2
Niko085 <nicolas.gajardo.p@usach.cl>
2024-06-10 04:51:51 -0400

    borrando lo anterior

--- ayudantia-microservicios-main/ms-orders/src/main/java/com/mingeso/msorders/repositories/OrderProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mingeso.msorders.repositories;
-
-import com.mingeso.msorders.entities.OrderProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderProductRepository extends JpaRepository<OrderProduct, Integer> {
-}
